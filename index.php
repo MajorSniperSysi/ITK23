9931202a7e68cff581a0644188a17101b6ef25fa
MajorSniperSysi <168183370+MajorSniperSysi@users.noreply.github.com>
2024-04-26 14:31:25 +0300

    Update index.php
    minion

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1>Meie projekti pealeht</h1>
     <p>See leht arvestab ka muudatustega</p>
-    <img src="https://https://www.cleverfiles.com/howto/it/what-is-jpg.html"
+    <img src="https://i.pinimg.com/736x/be/5b/9e/be5b9ea19da3c914fa676dfb5ab45624.jpg"
   </body>
 </html>
